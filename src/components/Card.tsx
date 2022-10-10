6daf09b7b603b6a560184d782f7bfe4eec8e06e9
ViGF <vitoriogdef@gmail.com>
2022-10-09 22:25:52 -0300

    Correção Tailwind V2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ export default function Card({ title, slug }: CardProps) {
             <div className="bg-main bg-cover bg-opacity-50 rounded-xl w-80 h-48 cursor-pointer hover:scale-105 transition-transform">
                 <div className="w-full h-full backdrop-brightness-50 p-5 rounded-xl">
                     <SLetter />
-                    <p className="font-bold text-4xl text-center mt-7">{title}</p>
+                    <p className="font-bold text-4xl text-center mt-6">{title}</p>
                 </div>
             </div>
         </Link>

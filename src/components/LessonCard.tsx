92c4526b4c13d50dd63e5b9653fadf7f074692eb
ViGF <vitoriogdef@gmail.com>
2022-10-09 22:39:57 -0300

    Img

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -17,7 +17,7 @@ export default function LessonCard({ title, aula, modulo, materia }: LessonCardP
 
     return (
         <div className="flex flex-row w-fit items-center bg-[#202020] mb-5">
-            <img src="../../module.png" alt="" className="brightness-50 hover:brightness-95" width={185} />
+            <img src="module.png" alt="" className="brightness-50 hover:brightness-95" width={185} />
             <p className="ml-5 mr-14 brightness-75 min-w-fit">{title}</p>
             <Link to={`/${materia}/${modulo}/${aula}`} >
                 <button className="flex flex-row items-center gap-3 text-black bg-white rounded-md text-base py-2 px-4 mr-6 hover:scale-110 transition-transform" onClick={reloadPage}>

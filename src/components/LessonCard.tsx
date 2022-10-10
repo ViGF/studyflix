f2c66b8b7fac0d9dfb0632338e480cfdc01d5f55
ViGF <vitoriogdef@gmail.com>
2022-10-09 22:45:10 -0300

    Alteração Loading

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -9,18 +9,12 @@ interface LessonCardProps {
 }
 
 export default function LessonCard({ title, aula, modulo, materia }: LessonCardProps) {
-    function reloadPage() {
-        setTimeout(() => {
-            window.location.reload()
-        }, 100);
-    }
-
     return (
         <div className="flex flex-row w-fit items-center bg-[#202020] mb-5">
             <img src="../../module.png" alt="" className="brightness-50 hover:brightness-95" width={185} />
             <p className="ml-5 mr-14 brightness-75 min-w-fit">{title}</p>
             <Link to={`/${materia}/${modulo}/${aula}`} >
-                <button className="flex flex-row items-center gap-3 text-black bg-white rounded-md text-base py-2 px-4 mr-6 hover:scale-110 transition-transform" onClick={reloadPage}>
+                <button className="flex flex-row items-center gap-3 text-black bg-white rounded-md text-base py-2 px-4 mr-6 hover:scale-110 transition-transform">
                     <Polygon />
                     Play
                 </button>
